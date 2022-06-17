12a11bd4895d1de730299077a139b674cd9a6f2b
Chelovek Dimka <tatuaj.romanyuk@gmail.com>
2022-06-18 01:18:14 +0300

    - Added missed files on the previous push

---
+++ js/boot.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.materialboxed'),
+        options = {},
+        instances = M.Materialbox.init(elems, options);
+});
